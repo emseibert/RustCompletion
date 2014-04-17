0011a84ba975665e23e918c132307cd8252da25f
Emily Seibert <ems4pc@virginia.edu>
2014-04-16 22:29:35 -0400

    create crate search methods

--- rustcompletion.py
+++ rustcompletion.py
@@ -12,17 +12,45 @@ class AllAutocomplete(sublime_plugin.EventListener):
 
     def on_query_completions(self, view, prefix, locations):
         line = [view.substr(sublime.Region(view.line(l).a, l)) for l in locations]
+        print("line")
+        print(line)
         commandsOnLine = line[0].split(' ')
-        lineToCall = commandsOnLine[len(commandsOnLine) - 1]
+        print("commandsOnLine")
+        print(commandsOnLine)
+        isCrate = ""
+
+        if (commandsOnLine[0] == "extern"):
+            lineToCall = "";
+            for s in commandsOnLine:
+                lineToCall += s + " "
+                isCrate = lineToCall
+            if len(commandsOnLine) > 2:
+                print("call this with crates:")
+                print (callRacerCrates(commandsOnLine[len(commandsOnLine) - 1]))
+
+                
+        else:
+            lineToCall = commandsOnLine[len(commandsOnLine) - 1]
+
+        print("lineToCall")
+        print(lineToCall)
         matches = callRacer(lineToCall)
         return matches
+
+def callRacerCrates(s):
+    os.environ['RUST_SRC_PATH'] = "/Users/emilyseibert/rust/src"
+    cmd = "cd /Users/emilyseibert/Library/'Application Support'/'Sublime Text 3'/Packages/CS4414FinalProject/racer/bin/; ./racer crate-sort " + s
+    (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+    results = stdout
+    return results
+
 
 def callRacer(s):
     os.environ['RUST_SRC_PATH'] = "/Users/emilyseibert/rust/src"
     #os.environ['RUST_SRC_PATH'] = '/home/student/rust/src'
     cmd = "cd /Users/emilyseibert/Library/'Application Support'/'Sublime Text 3'/Packages/CS4414FinalProject/racer/bin/; ./racer complete " + s
     #cmd = 'cd /home/student/CS4414FinalProject/racer/bin/; ./racer complete ' + s
-    cmd = 'cd "/home/student/.config/sublime-text-3/Packages/Complete/racer/bin/"; ./racer complete ' + s
+    #cmd = 'cd "/home/student/.config/sublime-text-3/Packages/Complete/racer/bin/"; ./racer complete ' + s
     (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
     results = []
     #print(stdout)
