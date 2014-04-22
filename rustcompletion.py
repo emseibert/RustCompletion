6c42a21d7a96d6eac2e5ad3c5ff11dc7dcd6c9e2
Kristen Felsing <klf2bf@virginia.edu>
2014-04-22 14:24:30 -0400

    Stopped sublime from matching single quotes after &

--- rustcompletion.py
+++ rustcompletion.py
@@ -16,12 +16,12 @@ class AllAutocomplete(sublime_plugin.EventListener):
         #gets the word completions from Sublime
         #could possibly turn off sublime auto complete, and manually put in the versions we want
         view_words = view.extract_completions(prefix, locations[0])
-        print("words" + str(view_words))
+        #print("words" + str(view_words))
 
         #gets region coordinates for all functions in text file
         function_regions = view.find_by_selector('meta.function')
         #list of regions
-        print(str(function_regions))
+        #print(str(function_regions))
         
         #gets whole line of function, doesn't include multi-lined fn
         #line = view.line(function_regions[2])
@@ -34,8 +34,8 @@ class AllAutocomplete(sublime_plugin.EventListener):
             strLine = view.substr(line)
             functionCompletions += functionParse(prefix, strLine)
 
-        print("function completions?")
-        print(functionCompletions)
+        #print("function completions?")
+        #print(functionCompletions)
         
 
         
@@ -70,11 +70,35 @@ class AllAutocomplete(sublime_plugin.EventListener):
         else:
             lineToCall = commandsOnLine[len(commandsOnLine) - 1]
             matches = callRacer(lineToCall)
-        #print("lineToCall")
-        #print(lineToCall)
+        
         matches_no_dup = without_duplicates(matches) + functionCompletions
-        print (matches_no_dup)
+        #print (matches_no_dup)
         return matches_no_dup
+
+    def on_modified(self, view):
+        if self.is_enabled():
+            for region in view.sel():  
+                # Only interested in empty regions, otherwise they may span multiple  
+                # lines, which doesn't make sense for this command.  
+                if region.empty():   
+                    # Expand the region to the full line it resides on, excluding the newline  
+                    line = view.line(region)
+                    lineContents = view.substr(line)
+                    if "&''" in lineContents:
+                        reg_m = re.match(r'(?P<begin>.*)&''',lineContents)
+                        if reg_m is not None:
+                            begin = reg_m.group('begin')
+                            new_line = begin + "&'"
+                            pos = view.sel()[0].begin()
+                            view.run_command("view_replace", { "begin" : view.sel()[0].begin()+1, "end": view.sel()[0].end(), "name": " " });
+                            
+
+    def is_enabled(self):
+        if str(sublime.active_window().active_view().settings().get('syntax')) == 'Packages/Rust/Rust.tmLanguage':
+            return True
+        else:
+            return False
+
 
 def callRacerCrates(s):
     RUST_SRC = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rust_src'))
@@ -88,7 +112,7 @@ def callRacerCrates(s):
 
 def functionParse(prefix, line):
     matches = []
-    print("FN PARSE")
+    #print("FN PARSE")
     fn = line.split("fn ")[1]
     if fn[0] == prefix:
         fn_name = line.split('(')[0]
@@ -106,15 +130,15 @@ def kfels_parse(prefix,line):
     return new_prefix
 
 def callRacer(s):
-    #rust_src = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rust_src'))
-    rust_src = "/Users/emilyseibert/rust/src"
+    rust_src = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rust_src'))
+    #rust_src = "/Users/emilyseibert/rust/src"
     cmd_loc = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'racer/bin'))
-
+    #print("S: " + s)
     cmd = 'cd "' + cmd_loc + '"; ./racer complete "' + rust_src + '" '+ s
-    print(cmd)
+    #print(cmd)
     (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
     results = []
-    print(results)
+    #print(results)
     limit = 0
     for line in stdout.splitlines():
         if limit > 5:
@@ -144,8 +168,8 @@ def callRacer(s):
             
             results.append(t)
             limit += 1
-
-   # print(results)
+    
+    #print(results)
     return results
 
 def parseLine(line):
@@ -199,3 +223,7 @@ def without_duplicates(words):
         if w not in result:
             result.append(w)
     return result
+
+class ViewReplaceCommand(sublime_plugin.TextCommand):
+  def run(self, edit, begin, end, name):
+    self.view.replace(edit, sublime.Region(begin, end), name)
