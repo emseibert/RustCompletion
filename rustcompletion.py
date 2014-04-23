1db3120a8be3020dab17405e643e5e49ad368af5
Kristen Felsing <klf2bf@virginia.edu>
2014-04-22 23:53:48 -0400

    Editted autocomplete for rust function and removed intial libc racer call

--- rustcompletion.py
+++ rustcompletion.py
@@ -11,69 +11,75 @@ import subprocess
 class AllAutocomplete(sublime_plugin.EventListener):
 
     def on_query_completions(self, view, prefix, locations):
+        if self.is_enabled():
+            functionCompletions = []
+            matches = []
 
-        functionCompletions = []
-        matches = []
+            line = [view.substr(sublime.Region(view.line(l).a, l)) for l in locations]
+            line_1 = line[0]
 
-        #gets the word completions from Sublime
-        view_words = view.extract_completions(prefix, locations[0])
-        
-        #gets region coordinates for all functions in text file
-        function_regions = view.find_by_selector('meta.function')
-        
-        #list of regions
-        #print(str(function_regions
+            if prefix == "":
+                return get_all_lifetimes(line_1)
 
-        list_of_fn_names = []        
-        #iterates over each function region and parses for function that have same prefix
-        for region in function_regions:
-            line = view.line(region)
-            strLine = view.substr(line)
-            list_of_fn_names.append(getFnNames(strLine))
-            functionCompletions += functionParse(prefix, strLine)
-
-        view_words = without_fn_dups(view_words, list_of_fn_names)
-
-        formatWords = []
-        for w in view_words:
-            t = (str(w), str(w))
-            formatWords.append(t)
-
-        matches += formatWords
-
-        line = [view.substr(sublime.Region(view.line(l).a, l)) for l in locations]
-        line_1 = line[0]
-        if prefix == 'use' and len(line_1)<3:
-            return [('use','use')]
-        if len(line_1)>2:
-            #Parse line to get all prior use's (let x = aa::bb::cc -> aa::bb)
-            res = kfels_parse(prefix, line)
-            if res is not '':
-                regions = view.find_all(r'use (.*?)::%s'%res,)
+            #gets the word completions from Sublime
+            view_words = view.extract_completions(prefix, locations[0])
             
-                if len(regions) is not 0:
-                    #print("region: " + view.substr(regions[0]))
-                    line_1 = view.substr(regions[0]) + '::' + prefix
-                    
+            #gets region coordinates for all functions in text file
+            function_regions = view.find_by_selector('meta.function')
+            
+            #list of regions
+            #print(str(function_regions
 
-        commandsOnLine = line_1.split(' ')
-        isCrate = ""
+            list_of_fn_names = []        
+            #iterates over each function region and parses for function that have same prefix
+            for region in function_regions:
+                line2 = view.line(region)
+                strLine = view.substr(line2)
+                list_of_fn_names.append(getFnNames(strLine))
+                functionCompletions += functionParse(prefix, strLine)
 
-        if (commandsOnLine[0] == "extern"):
-            lineToCall = "";
-            for s in commandsOnLine:
-                lineToCall += s + " "
-                isCrate = lineToCall
-            if len(commandsOnLine) > 2:
-                matches += callRacerCrates(commandsOnLine[len(commandsOnLine) - 1])         
-        else:
-            lineToCall = commandsOnLine[len(commandsOnLine) - 1]
-            matches += callRacer(lineToCall)
-        #print("lineToCall")
-        #print(lineToCall)
-        matches_no_dup = functionCompletions + without_duplicates(matches)
+            view_words = without_fn_dups(view_words, list_of_fn_names)
 
-        return matches_no_dup
+            formatWords = []
+            for w in view_words:
+                t = (str(w), str(w))
+                formatWords.append(t)
+
+            matches += formatWords
+
+            if prefix == "libc":
+                return[('','')]
+            if prefix == 'use' and len(line_1)<3:
+                return [('use','use')]
+            if len(line_1)>2:
+                #Parse line to get all prior use's (let x = aa::bb::cc -> aa::bb)
+                res = kfels_parse(prefix, line)
+                if res is not '':
+                    regions = view.find_all(r'use (.*?)::%s'%res,)
+                
+                    if len(regions) is not 0:
+                        #print("region: " + view.substr(regions[0]))
+                        line_1 = view.substr(regions[0]) + '::' + prefix
+                        
+
+            commandsOnLine = line_1.split(' ')
+            isCrate = ""
+
+            if (commandsOnLine[0] == "extern"):
+                lineToCall = "";
+                for s in commandsOnLine:
+                    lineToCall += s + " "
+                    isCrate = lineToCall
+                if len(commandsOnLine) > 2:
+                    matches += callRacerCrates(commandsOnLine[len(commandsOnLine) - 1])         
+            else:
+                lineToCall = commandsOnLine[len(commandsOnLine) - 1]
+                matches += callRacer(lineToCall)
+            #print("lineToCall")
+            #print(lineToCall)
+            matches_no_dup = functionCompletions + without_duplicates(matches)
+
+            return matches_no_dup
 
     def on_modified(self, view):
         if self.is_enabled():
@@ -84,14 +90,26 @@ class AllAutocomplete(sublime_plugin.EventListener):
                     # Expand the region to the full line it resides on, excluding the newline  
                     line = view.line(region)
                     lineContents = view.substr(line)
-                    if "&''" in lineContents:
-                        reg_m = re.match(r'(?P<begin>.*)&''',lineContents)
-                        if reg_m is not None:
-                            begin = reg_m.group('begin')
-                            new_line = begin + "&'"
-                            pos = view.sel()[0].begin()
-                            view.run_command("view_replace", { "begin" : view.sel()[0].begin()+1, "end": view.sel()[0].end(), "name": " " });
-                            
+                    if "fn" in lineContents:
+                        if "&''" in lineContents:
+                            reg_m = re.match(r'(?P<begin>.*)&''',lineContents)
+                            if reg_m is not None:
+                                begin = reg_m.group('begin')
+                                pos = view.sel()[0].begin()
+                                view.run_command("view_replace", { "begin" : view.sel()[0].begin()+1, "end": view.sel()[0].end(), "name": " " });
+                        if "<''" in lineContents:
+                            reg_m = re.match(r'(?P<begin>.*)<''',lineContents)
+                            if reg_m is not None:
+                                begin = reg_m.group('begin')
+                                pos = view.sel()[0].begin()
+                                view.run_command("view_replace", { "begin" : view.sel()[0].begin()+1, "end": view.sel()[0].end(), "name": " " });
+                        elif "<'" in lineContents:
+                            reg_m = re.match(r"(?P<begin>.*fn.*<'[a_zA_Z](,[\s]*'[\s]*(\w))*),[\s]*''", lineContents)
+                            if reg_m is not None:
+                                begin = reg_m.group('begin')
+                                pos = view.sel()[0].begin()
+                                view.run_command("view_replace", { "begin" : view.sel()[0].begin()+1, "end": view.sel()[0].end(), "name": " " });
+                                 
 
     def is_enabled(self):
         if str(sublime.active_window().active_view().settings().get('syntax')) == 'Packages/Rust/Rust.tmLanguage':
@@ -106,7 +124,7 @@ def without_fn_dups(sublimeList, fnList):
         if s not in fnList:
             matches.append(s)
 
-    print(matches)
+    #print(matches)
     return matches
 
 def getFnNames(strLine):
@@ -193,7 +211,7 @@ def callRacer(s):
                 continue
 
             #remove commented code and test functions from results
-            if test.startswith('//') or test.startswith('test!('): #remove commented code and test functions
+            if test.startswith('//') or test.startswith('test!(') or test.startswith('iotest!'): #remove commented code and test functions
                 #print(test + " is a comment")
                 continue
             #print("before: " + line)
@@ -235,28 +253,31 @@ def parseLine(line):
             result = splitLine[2].split('<')[0] + ";"
         elif (splitLine[1].strip()=='unsafe'):
             if (splitLine[2].strip()=='fn'):
-                if splitLine[3].strip().find('<') > -1:
-                    result = splitLine[3].strip().split('<')[0] + "()"
-                else:
-                    result = splitLine[3].split('(')[0].strip() + "()"
+                result = parse_rust_func(line)
     if (splitLine[0].strip()=="fn"):
-            if splitLine[1].strip().find('<') > -1:
-                result = splitLine[1].strip().split('<')[0] + "()"
-            else:
-                result = splitLine[1].split('(')[0].strip() + "()"
+            result = parse_rust_func(line)
     if (splitLine[1].strip()=="fn"):
-            if splitLine[2].find('<') > -1:
-                result = splitLine[2].strip().split('<')[0] + "()"
-            else:
-                result = splitLine[2].split('(')[0].strip() + "()"
+            result = parse_rust_func(line)
     return result 
 
-# keeps first instance of every word and retains the original order
-def without_duplicates(words):
-    result = []
-    for w in words:
-        if w not in result:
-            result.append(w)
+def parse_rust_func(line):
+    result = line
+    reg = re.match(r'(?:pub)*(?:\s)*(?:unsafe)*(?:\s)*(?:fn)\s*(?P<func>\b(?:\w)*\b(?:<(?:.*)>)*)(?:\((?P<args>[^)]*)\))(?:.*)(?:{)*(?:;)*', line)
+    if reg is not None:
+        func = reg.group('func') + "("
+        result = func
+        func_list = func.split('<')
+        if len(func_list) >1:
+            result = func_list[0] + "("
+        args = reg.group('args')
+        args_list = args.split(',')
+        if len(args_list) ==1:
+            result += (args_list[0].split(':')[0])
+            args_list.pop(0)
+        if len(args_list) >= 1:
+            for x in args_list:
+                result += ("," + x.split(':')[0])
+        result += (")")
     return result
 
 # keeps first instance of every word and retains the original order
@@ -267,6 +288,16 @@ def without_duplicates(words):
             result.append(w)
     return result
 
+def get_all_lifetimes(line):
+    result = []
+    line_2 = line.split('<')
+    if len(line_2)>1:
+        line_3 = line_2[1].split('>')
+        line_4 = line_3[0].split(',')
+        for x in line_4:
+            result.append((x.strip(),x.strip()))
+    return result
+    
 class ViewReplaceCommand(sublime_plugin.TextCommand):
   def run(self, edit, begin, end, name):
     self.view.replace(edit, sublime.Region(begin, end), name)
